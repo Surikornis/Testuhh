1c8dc0c42c8390960b301c5dd662a3e4f74e4722
kristof.suri@gmail.com <kristof.suri@gmail.com>
2024-02-25 17:59:48 +0100

    asd

---
+++ Backend/SocialAnalytics/Controllers/Empty.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialAnalytics.Controllers
+{
+    public class Empty : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
